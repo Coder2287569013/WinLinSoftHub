3d9f5de358adbfe12c9f5857a36483c840c341d1
= <yarovenkovova2008@gmail.com>
2024-02-10 12:03:11 -0330

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
-import fs from 'fs';
 import vue from '@vitejs/plugin-vue'
-
-const pathToCert = '../certs/dev.local+4.pem';
-const pathToKey = '../certs/dev.local+4-key.pem';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,12 +10,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    https: {
-      key: fs.readFileSync(pathToKey),
-      cert: fs.readFileSync(pathToCert),
     }
   }
 });
